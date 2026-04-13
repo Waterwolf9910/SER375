ae288f8944b146542b4de9d57adab98010aa1858
aditibaghel9 <asbaghel@quinnipiac.edu>
2026-04-13 17:44:40 -0400

    Re add missing changes

--- game/Scripts/temp_Movement.cs
+++ game/Scripts/temp_Movement.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class temp_Movement : Sprite2D {
+public partial class temp_Movement : AnimatedSprite2D {
     [Export]
     public int speed { get; set; } = 5;
     // Called when the node enters the scene tree for the first time.
@@ -10,16 +10,16 @@ public partial class temp_Movement : Sprite2D {
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta) {
-        if (Input.IsActionPressed("up")) {
+        if (Input.IsActionPressed("Up")) {
             this.Position = new Vector2(this.Position.X, Math.Max(this.GetViewportRect().Position.Y, this.Position.Y - this.speed));
         }
-        if (Input.IsActionPressed("down")) {
+        if (Input.IsActionPressed("Down")) {
             this.Position = new Vector2(this.Position.X, Math.Min(this.GetViewportRect().End.Y, this.Position.Y + this.speed));
         }
-        if (Input.IsActionPressed("left")) {
+        if (Input.IsActionPressed("Left")) {
             this.Position = new Vector2(Math.Max(this.GetViewportRect().Position.X, this.Position.X - this.speed), this.Position.Y);
         }
-        if (Input.IsActionPressed("right")) {
+        if (Input.IsActionPressed("Right")) {
             this.Position = new Vector2(Math.Min(this.GetViewportRect().End.X, this.Position.X + this.speed), this.Position.Y);
         }
     }
